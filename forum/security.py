69168db16710636f93d61f7a881b8f4eeff0df32
kali <kali@kali.kali_kali.com>
2026-01-11 08:02:42 -0500

    L7.2

---
+++ forum/security.py
@@ -0,0 +1,9 @@
+import logging
+
+security_logger = logging.getLogger("security")
+
+def get_client_ip(request):
+    xff = request.META.get("HTTP_X_FORWARDED_FOR")
+    if xff:
+        return xff.split(",")[0].strip()
+    return request.META.get("REMOTE_ADDR")
